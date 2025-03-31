4b89c7facd24acfd9a97a784320499c1ff86359a
Cole Patterson <colecpatterson@gmail.com>
2025-03-30 22:00:58 -0400

    Refactored

--- part1/courseinfo(1.3)/src/App.jsx
+++ part1/courseinfo(1.3)/src/App.jsx
@@ -1,45 +1,25 @@
-const Header = (props)=> {
-  return (
-   <div>
-    <h1>{props.course}</h1>
-   </div>
-  )
-}
-
-const Content = () => {
-  const Part = (props) => {
-    return (
-      <div>
-        <p>{props.part} {props.exercises}</p>
-      </div>
-    )
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const total = (part1.exercises + part2.exercises + part3.exercises)
   return (
     <div>
-      <Part part="Fundamentals of React" exercises={10} />
-      <Part part="Using props to pass data" exercises={7} />
-      <Part part="State of a component" exercises={14} />
-    </div>
-  )
-
-}
-
-const Total = ()=> {
-  return (
-    <div>
-      <p>Number of exercises {10 + 7 + 14}</p>
-    </div>
-  )
-
-}
-
-const App = () => { 
-
-  return (
-    <div>
-      <Header course="Half Stack application development" />
-      <Content />
-      <Total />
+      <h1>{course}</h1>
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
